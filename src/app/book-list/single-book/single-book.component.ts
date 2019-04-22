a3b9fcd0d56a2f51a5eb7fac84bd4605ce3ecb8e
gfavre <guillaumefavre56@gmail.com>
2019-04-22 19:30:30 +0200

    Ajout, suppression de livres

--- src/app/book-list/single-book/single-book.component.ts
+++ src/app/book-list/single-book/single-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../../models/book.model';
+import { BooksService } from '../../services/books.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-single-book',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleBookComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
+
+  constructor(private route: ActivatedRoute,
+  			  private booksService: BooksService,
+  			  private router: Router) { }
 
   ngOnInit() {
+  	// Création d'un book vide temporaire (en attendant le chargement depuis la BDD)
+  	this.book = new Book('', '');
+  	const id = this.route.snapshot.params['id'];
+  	this.booksService.getSingleBook(+id).then(
+  		(book: Book) => {
+  			this.book = book;
+  		}
+	);
+  }
+
+  onBack() {
+  	this.router.navigate(['/books']);
   }
 
 }
